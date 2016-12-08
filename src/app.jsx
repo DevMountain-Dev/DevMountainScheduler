2aa7f732084403fbfbc94831fe554dfa65e303ea
Scott Gourley <scottgourley9@gmail.com>
2016-12-08 13:31:03 -0700

    add functionality to create template

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
-
+import NavBar from './components/NavBar/NavBar.jsx';
+import CreateTemplate from './components/CreateTemplate/CreateTemplate.jsx'
+import Settings from './components/Settings/Settings.jsx'
 
 class App extends React.Component{
   render() {
     return (
       <div>
-      its working
+      <Router history={hashHistory}>
+              <Route component={NavBar}>
+                  <Route path="/" />
+                  <Route path="/CreateTemplate" component={CreateTemplate} />
+                  <Route path="/Settings" component={Settings.jsx}/>
+              </Route>
+          </Router>
       </div>
 
     )
